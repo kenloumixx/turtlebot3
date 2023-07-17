b7bbb2ce233e3b193f3d6ec9597975febf9c53f5
kenloumixx <kenloumixx@gmail.com>
2023-07-17 11:48:38 +0900

    fix: Change name of container

--- env/scripts/docker_run.sh
+++ env/scripts/docker_run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run --name desktop-ros2-humble-new-4 -d -it --rm -e "ACCEPT_EULA=Y" --ipc=host --privileged --network=host \
+docker run --name turtle-humble -d -it -e "ACCEPT_EULA=Y" --ipc=host --privileged --network=host \
 -v $(pwd)/../:/workspace:rw \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -e DISPLAY=$DISPLAY \
